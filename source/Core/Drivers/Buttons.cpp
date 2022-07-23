7400bc058f2c02f489084d02313f098b039b5231
nyanpasu64 <nyanpasu64@tuta.io>
2022-07-22 22:16:06 -0700

    Rewrite getButtonState() to cleanup logic and fix bugs
    In addition to increasing readability at the cost of increased length,
    this changes button behavior in subtle ways:
    
    * Fixed: Every single-button press generates either one press-release
    event or 1+ long-press events (assuming I didn't write any bugs).
    Previously if you didn't hold a button until the first check at >timeout
    ticks, it would never trigger BUTTON_*_LONG, and if you released it
    after the last check at <timeout ticks, it would not trigger
    BUTTON_*_SHORT.
    
    * Changed: Do not generate a spurious BUTTON_BOTH when releasing a
    two-button long-press. Previously, if you released the two buttons not
    at the same time, the first release would update previousStateChange,
    and the second release would misinterpret previousStateChange as a
    button *press* time and act like a two-button short-press. If desired,
    the new code could be changed to *always* generate BUTTON_BOTH releasing
    a two-button long-press.
    
    * Changed: When two buttons are long-pressed and one is released, the
    other button registers a long-press again after a delay. The old code
    would not generate any events afterwards. This change is not necessarily
    desirable, and can be reverted by removing State::state and only keeping
    totalState.
    
    * If you want adding buttons to a long-press to instantly generate
    BUTTON_BOTH_LONG, you must not update stateChangeTime when pressing a
    second button, and instead fallthrough to the BUTTON_*_LONG logic.

--- source/Core/Drivers/Buttons.cpp
+++ source/Core/Drivers/Buttons.cpp
@@ -22,21 +22,103 @@ ButtonState getButtonState() {
    * press (buttons still down), or if release we trigger press
    * (downtime>filter)
    */
-  static uint8_t  previousState       = 0;
-  static uint32_t previousStateChange = 0;
-  const uint16_t  timeout             = TICKS_100MS * 4;
-  uint8_t         currentState;
-  currentState = (getButtonA()) << 0;
-  currentState |= (getButtonB()) << 1;
+  constexpr uint16_t LONG_PRESS_MIN = TICKS_100MS * 4;
 
+  struct State {
+    /// Bitmask for currently held buttons. Used to identify buttons being
+    /// pressed or released.
+    uint8_t state = 0;
+
+    /// Set on every call (aside from consecutive no-button-press calls) to
+    /// (state.state && now - state.initialPressTime < LONG_PRESS_MIN). Only
+    /// read when releasing all buttons.
+    bool wasShortPress = false;
+
+    /// Bitmask for any buttons pressed, cleared when releasing all buttons.
+    /// Only read when releasing all buttons in a short-press. Zero if
+    /// previousState is 0.
+    uint8_t totalState = 0;
+
+    /// Only set when transitioning from no buttons pressed to buttons pressed.
+    /// Used to calculate wasShortPress.
+    TickType_t initialPressTime = 0;
+
+    /// Set whenever any buttons are pressed or released (state is changed).
+    /// Used to delay sending long-press events.
+    TickType_t stateChangeTime = 0;
+  };
+  static State state;
+
+  const uint8_t currentState = (getButtonA() << 0) | (getButtonB() << 1);
+  const TickType_t now = xTaskGetTickCount();
   if (currentState)
-    lastButtonTime = xTaskGetTickCount();
-  if (currentState == previousState) {
+    lastButtonTime = now;
+
+  // If a button is newly pressed, assign state.initialPressTime here.
+  // state.initialPressTime is used to compute isShortPress ->
+  // state.wasShortPress.
+  if (currentState && !state.state) {
+    state.initialPressTime = now;
+  }
+
+  // If any button is held >= LONG_PRESS_MIN, don't generate a short press event
+  // on release.
+  const bool isShortPress =
+    currentState && (now - state.initialPressTime < LONG_PRESS_MIN);
+
+  if (currentState != state.state) {
+    // A button is pressed or released.
+    state.stateChangeTime = now;
+
+    if (currentState) {
+      // Buttons are held. Return BUTTON_NONE (nothing is done until all buttons
+      // are released), but log all buttons pressed to totalState. If both
+      // buttons were pressed and one is released, return BUTTON_BOTH once both
+      // are released.
+      state.state = currentState;
+      state.wasShortPress = isShortPress;
+      state.totalState = static_cast<uint8_t>(state.totalState | currentState);
+      return BUTTON_NONE;
+    } else {
+      // User has released all buttons. If we have (loosely speaking) not
+      // returned a long-press event, return a short-press event.
+      //
+      // Checking state.wasShortPress (not isShortPress) ensures that every
+      // single-button press returns either a short event or 1+ long events.
+
+      ButtonState retVal = BUTTON_NONE;
+      if (state.wasShortPress) {
+        // The user didn't hold the button for long
+        // So we send button press
+
+        if (state.totalState == 0x01)
+          retVal = BUTTON_F_SHORT;
+        else if (state.totalState == 0x02)
+          retVal = BUTTON_B_SHORT;
+        else
+          retVal = BUTTON_BOTH; // Both being held case
+      }
+      state.state = currentState;
+      state.wasShortPress = isShortPress;
+      state.totalState = 0;
+      return retVal;
+    }
+  } else {
+    // Button state unchanged.
+    // If no buttons are pressed, exit.
     if (currentState == 0)
       return BUTTON_NONE;
-    if ((xTaskGetTickCount() - previousStateChange) > timeout) {
-      // User has been holding the button down
-      // We want to send a button is held message
+
+    // Buttons are pressed.
+    state.wasShortPress = isShortPress;
+
+    // If the current exact state is held >= LONG_PRESS_MIN, generate a long
+    // press event.
+    //
+    // Checking state.stateChangeTime (not state.initialPressTime) ensures that
+    // releasing one of two held buttons doesn't immediately generate an
+    // unwanted BUTTON_F_LONG or BUTTON_B_LONG event.
+    if (now - state.stateChangeTime >= LONG_PRESS_MIN) {
       if (currentState == 0x01)
         return BUTTON_F_LONG;
       else if (currentState == 0x02)
@@ -45,40 +127,7 @@ ButtonState getButtonState() {
         return BUTTON_BOTH_LONG; // Both being held case
     } else
       return BUTTON_NONE;
-  } else {
-    // A change in button state has occurred
-    ButtonState retVal = BUTTON_NONE;
-    if (currentState) {
-      // User has pressed a button down (nothing done on down)
-      if (currentState != previousState) {
-        // There has been a change in the button states
-        // If there is a rising edge on one of the buttons from double press we
-        // want to mask that out As users are having issues with not release
-        // both at once
-        if (previousState == 0x03)
-          currentState = 0x03;
-      }
-    } else {
-      // User has released buttons
-      // If they previously had the buttons down we want to check if they were <
-      // long hold and trigger a press
-      if ((xTaskGetTickCount() - previousStateChange) < timeout) {
-        // The user didn't hold the button for long
-        // So we send button press
-
-        if (previousState == 0x01)
-          retVal = BUTTON_F_SHORT;
-        else if (previousState == 0x02)
-          retVal = BUTTON_B_SHORT;
-        else
-          retVal = BUTTON_BOTH; // Both being held case
-      }
-    }
-    previousState       = currentState;
-    previousStateChange = xTaskGetTickCount();
-    return retVal;
   }
-  return BUTTON_NONE;
 }
 
 void waitForButtonPress() {
